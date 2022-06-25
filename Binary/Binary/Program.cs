f807e9f2f2d29b7a095b328bac6f53bab7b14b67
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-25 15:31:03 +0530

    adding data to tree

---
+++ Binary/Binary/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Day19_BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree Problem");
+            BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+            ///UC1: Add the node in the tree and it doesnot have any output just display welcome message
+
+            BinaryTree.AddNode(56);
+            BinaryTree.AddNode(30);
+            BinaryTree.AddNode(70);
+            BinaryTree.AddNode(22);
+            BinaryTree.AddNode(40);
+            BinaryTree.AddNode(11);
+            BinaryTree.AddNode(63);
+            BinaryTree.AddNode(67);
+            BinaryTree.AddNode(16);
+            BinaryTree.AddNode(60);
+            BinaryTree.AddNode(95);
+            BinaryTree.AddNode(65);
+            BinaryTree.AddNode(3);
+            //UC2:create the bst as shown and find the size of tree
+
+            int Size = BinaryTree.GetSize();
+            Console.WriteLine(Size);
+            //uc3:Ability to search the given node
+            bool result = BinaryTree.SearchKey(63);
+            Console.WriteLine(result);
+            //output
+            //Welcome To Binary Search Tree Problem
+            //13
+            //True
+
+
+        }
+    }
+}
